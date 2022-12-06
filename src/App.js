9b07af6f2b927c7ed1248228d9aba18554411bba
fatimajcm <fatimamacias500@outlook.com>
2022-12-05 18:13:00 -0600

    ..

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="App">
 
-      <Router>
+  <Router>
         <Routes>
           
           <Route path="/chat/:user" element={<><Header backBtn="/chats"/><IndividualChat/></>}>
